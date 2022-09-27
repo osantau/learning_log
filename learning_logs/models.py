5e9bb026bd4342e1837b0f4c047872746f58b59e
octsantau <octavian.santau@plastor.ro>
2022-09-27 08:36:29 +0300

    Rename date_add to created. Add updated field to columns.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,7 +6,8 @@ from django.db import models
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
-    date_added = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         """Return a string representation of the model"""
@@ -17,7 +18,8 @@ class Entry(models.Model):
     """Something specific learnd about a topic"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'entries'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic,Entry
+from .models import Topic, Entry
 
 # Create your views here.
 
@@ -8,15 +8,17 @@ def index(request):
     """The home page for Learning Log"""
     return render(request, 'learning_logs/index.html')
 
+
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.all().order_by('date_added')
-    context = {'topics':topics}
-    return render(request, 'learning_logs/topics.html',context)
+    topics = Topic.objects.all().order_by('created')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
 
 def topic(request, topic_id):
     """Show a topic details"""
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
-    context = {'topic':topic,'entries':entries}
-    return render(request, 'learning_logs/topic.html',context)
+    entries = topic.entry_set.order_by('-created')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
