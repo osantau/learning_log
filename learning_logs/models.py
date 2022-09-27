959282caafa7a4b92b197e81355b51edca237e5e
octsantau <octavian.santau@plastor.ro>
2022-09-27 11:13:35 +0300

    Protecting pages

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model"""
